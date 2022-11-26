6e32514e982318236177f2f4258ec27600f6fddc
Richard Slaughter <richardslaughter@DESKTOP-FFFB0SU.localdomain>
2022-11-26 23:15:22 +0000

    enable NAT gateway

--- main.tf
+++ main.tf
@@ -63,10 +63,10 @@ module "vpc" {
     Tier = "tgw"
   }
 
-  enable_nat_gateway     = false
+  enable_nat_gateway     = true
   enable_vpn_gateway     = false
   one_nat_gateway_per_az = false
-  single_nat_gateway     = false
+  single_nat_gateway     = true
 
   enable_flow_log                                 = false
 }
